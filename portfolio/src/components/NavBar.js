4a43c6b358a01aacdd8aa15a4860f7bac8e60184
bleufox <thebleufox8@gmail.com>
2021-10-24 15:27:28 -0700

    Working on nav bar CSS

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,40 +4,41 @@ function NavBar({ currentPage, handlePageChange }) {
 
 
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <a
-                    href="#aboutme"
-                    onClick={() => handlePageChange('AboutMe')}
-                    className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
-                >
-                    About Me
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#portfolio"
-                    onClick={() => handlePageChange('Portfolio')}
-                    //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-                    //Conditional statement to determine what to display
-                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                    Portfolio
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#contact"
-                    onClick={() => handlePageChange('Contact')}
-                    //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-                    //Conditional statement to determine what to display
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </a>
-            </li><li className="nav-item">
-            </li>
-        </ul>
+        <div class="container">
+            <ul className="nav nav-tabs">
+                <li className="nav-item"><span>
+                    <a
+                        href="#aboutme"
+                        onClick={() => handlePageChange('AboutMe')}
+                        className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+                    >
+                        About Me
+                    </a>
+                </span>
+                </li>
+                <li className="nav-item"><span>
+                    <a
+                        href="#portfolio"
+                        onClick={() => handlePageChange('Portfolio')}
+                        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Portfolio
+                    </a>
+                </span>
+                </li>
+                <li className="nav-item"><span>
+                    <a
+                        href="#contact"
+                        onClick={() => handlePageChange('Contact')}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Contact
+                    </a>
+                </span>
+                </li><li className="nav-item">
+                </li>
+            </ul>
+        </div>
     )
 }
 
