72d455b6b3c7bae597064f79e103eac04a98e93f
bleufox <thebleufox8@gmail.com>
2021-10-24 22:27:07 -0700

    Finished background image sizing

--- portfolio/src/page/Contact.js
+++ portfolio/src/page/Contact.js
@@ -1,12 +1,80 @@
+// import React from 'react';
+
+// function Contact() {
+//     return (
+//         <React.Fragment>
+//             <section id="portfolio" className="projects">
+//                 <div className="container">
+//                     <div className="grid">
+//                         <div className="contact-info">
+//                             <h3>Email</h3>
+//                             <h4><a href="mailto:thebleufox8@gmail.com">thebleufox8@gmail.com</a></h4>
+//                         </div>
+//                         <div class="contact-info">
+//                             <h3>GitHub</h3>
+//                             <h4><a href="https://github.com/bleufox" target="_blank" rel="noreferrer">@bleufox</a></h4>
+//                         </div>
+//                         <div class="contact-info">
+//                             <h3>GitHub</h3>
+//                             <h4><a href="https://github.com/bleufox" target="_blank" rel="noreferrer">@bleufox</a></h4>
+//                         </div>
+//                 </div>
+//             </div>
+//         </section>
+//         </React.Fragment >
+//     )
+// }
+
+// export default Contact;
+
 import React from 'react';
 
-function Contact() {
+function Projects() {
     return (
-        <div>
-            <h2>THIS IS HOW YOU CONTACT ME</h2>
-            <h4>Email: <a href="mailto:thebleufox8@gmail.com">thebleufox8@gmail.com</a></h4>
-        </div>
+        <React.Fragment>
+            <section id="portfolio" className="projects">
+                <div className="container">
+                    <div className="grid">
+                        <a href="mailto:thebleufox8@gmail.com">
+                            <div className="project">
+                                <h3>Email</h3>
+                                <h2>thebleufox8@gmail.com</h2>
+                            </div>
+                        </a>
+                        <a href="https://github.com/bleufox" target="blank" rel="noreferrer">
+                            <div className="project">
+                                <h3>GitHub</h3>
+                                <h2>@bleufox</h2>
+                            </div>
+                        </a>
+                        <a href="https://www.linkedin.com/in/bleufox/" target="blank" rel="noreferrer">
+                            <div className="project">
+                                <h3>LinkedIn</h3>
+                                <h2>@bleufox</h2>
+                            </div>
+                        </a>
+                        <a href="https://drive.google.com/file/d/1upQ6iPjUo1oXvPEMFhn8QCgEl-7bJ3Cl/view?usp=sharing" target="blank" rel="noreferrer">
+                            <div className="project">
+                                <h3>Resume</h3>
+                            </div>
+                        </a>
+                        <a href="mailto:thebleufox8@gmail.com">
+                            <div className="project">
+                                <h3>LinkedIn</h3>
+                                <h2>thebleufox8@gmail.com</h2>
+                            </div>
+                        </a>
+                        <a href="mailto:thebleufox8@gmail.com">
+                            <div className="project">
+                                <h3>LinkedIn</h3>
+                                <h2>thebleufox8@gmail.com</h2>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
     )
 }
 
-export default Contact
+export default Projects;
--- portfolio/src/page/Projects.js
+++ portfolio/src/page/Projects.js
@@ -9,43 +9,37 @@ function Projects() {
                         <a href="https://trails-or-fails-app.herokuapp.com/" target="_blank" rel="noreferrer">
                             <div className="project">
                                 <h3>Trails or Fails</h3>
+                                <h2>Heroku App</h2>
                             </div>
                         </a>
                         <a href="https://piano-on-the-go.herokuapp.com/" target="_blank" rel="noreferrer">
                             <div class="project">
                                 <h3>Piano on the Go!</h3>
+                                <h2>Heroku App</h2>
                             </div>
                         </a>
-                        <a href="https://gibsona475.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
+                        <a href="https://bleufox.github.io/BleusDayPlanner/" target="_blank" rel="noreferrer">
                             <div class="project">
-                                <h3>
-                                    Weather <br /> Dashboard
-                                    <h4>(Search by city weather info)</h4>
-                                </h3>
+                                <h3>Bleu's Day Planner</h3>
+                                <h2>jQuery</h2>
                             </div>
                         </a>
-                        <a href="https://www.canva.com/design/DAEN1jAJqSg/jTxJa3Q8JYvuq1z2B2lNAQ/view?utm_content=DAEN1jAJqSg&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" target="_blank" rel="noreferrer">
+                        <a href="https://bleufox.github.io/Personal-Library/" target="_blank" rel="noreferrer">
                             <div class="project">
-                                <h3>
-                                    My <br /> Resume
-                                    <h4>(My up to date resume )</h4>
-                                </h3>
+                                <h3>Personal Library</h3>
+                                <h2>API</h2>
                             </div>
                         </a>
-                        <a href="https://www.linkedin.com/in/anthony-gibson-aaa7aa157/" target="_blank" rel="noreferrer">
+                        <a href="https://bleufox.github.io/BleusEmployeeTracker/" target="_blank" rel="noreferrer">
                             <div class="project">
-                                <h3>
-                                    My <br /> LinkedIn
-                                    <h4>(My up to date LinkedIn)</h4>
-                                </h3>
+                                <h3>Employee Tracking System</h3>
+                                <h2>SQL</h2>
                             </div>
                         </a>
-                        <a href="https://github.com/gibsona475" target="_blank" rel="noreferrer">
+                        <a href="https://github.com/bleufox/BleusNoteTaker" target="_blank" rel="noreferrer">
                             <div class="project">
-                                <h3>
-                                    My <br /> GitHub
-                                    <h4>(My up to date GitHub)</h4>
-                                </h3>
+                                <h3>Note Taker</h3>
+                                <h2>Express Server</h2>
                             </div>
                         </a>
                     </div>
