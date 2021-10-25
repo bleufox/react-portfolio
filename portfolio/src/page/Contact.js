ef70730f92dd2ff1d471fe822b648596c7ddbfd4
bleufox <thebleufox8@gmail.com>
2021-10-24 23:21:37 -0700

    Finalized contact page

--- portfolio/src/page/Contact.js
+++ portfolio/src/page/Contact.js
@@ -13,19 +13,11 @@ function Projects() {
                         <a href="mailto:thebleufox8@gmail.com">
                             <div className="project">
                                 <h3>Email</h3>
-                                <h2>thebleufox8@gmail.com</h2>
-                            </div>
-                        </a>
-                        <a href="https://github.com/bleufox" target="blank" rel="noreferrer">
-                            <div className="project">
-                                <h3>GitHub</h3>
-                                <h2>@bleufox</h2>
                             </div>
                         </a>
                         <a href="https://www.linkedin.com/in/bleufox/" target="blank" rel="noreferrer">
                             <div className="project">
                                 <h3>LinkedIn</h3>
-                                <h2>@bleufox</h2>
                             </div>
                         </a>
                         <a href="https://drive.google.com/file/d/1upQ6iPjUo1oXvPEMFhn8QCgEl-7bJ3Cl/view?usp=sharing" target="blank" rel="noreferrer">
@@ -33,16 +25,14 @@ function Projects() {
                                 <h3>Resume</h3>
                             </div>
                         </a>
-                        <a href="mailto:thebleufox8@gmail.com">
+                        <a href="https://github.com/bleufox" target="blank" rel="noreferrer">
                             <div className="project">
-                                <h3>LinkedIn</h3>
-                                <h2>thebleufox8@gmail.com</h2>
+                                <h3>GitHub</h3>
                             </div>
                         </a>
-                        <a href="mailto:thebleufox8@gmail.com">
+                        <a href="https://www.instagram.com/juniperinwanderland/" target="blank" rel="noreferrer">
                             <div className="project">
-                                <h3>LinkedIn</h3>
-                                <h2>thebleufox8@gmail.com</h2>
+                                <h3>Instagram</h3>
                             </div>
                         </a>
                     </div>
