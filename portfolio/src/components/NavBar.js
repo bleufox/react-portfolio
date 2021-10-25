7a776b086a65dc60d0cd369ef95e73002ea7cdde
bleufox <thebleufox8@gmail.com>
2021-10-24 19:44:08 -0700

    Multiple changes: CSS, projects grid links started

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -3,41 +3,43 @@ import React from 'react';
 function NavBar({ currentPage, handlePageChange }) {
 
     return (
-        <div class="container">
-            <ul className="nav nav-tabs">
-                <li className="nav-item"><span>
-                    <a
-                        href="#aboutme"
-                        onClick={() => handlePageChange('AboutMe')}
-                        className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
-                    >
-                        About Me
-                    </a>
-                </span>
-                </li>
-                <li className="nav-item"><span>
-                    <a
-                        href="#projects"
-                        onClick={() => handlePageChange('Projects')}
-                        className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-                    >
-                        Projects
-                    </a>
-                </span>
-                </li>
-                <li className="nav-item"><span>
-                    <a
-                        href="#contact"
-                        onClick={() => handlePageChange('Contact')}
-                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                    >
-                        Contact
-                    </a>
-                </span>
-                </li><li className="nav-item">
-                </li>
-            </ul>
-        </div>
+        <section class='navbar-container'>
+            <div class="container">
+                <ul className="nav nav-tabs">
+                    <li className="nav-item"><span>
+                        <a
+                            href="#aboutme"
+                            onClick={() => handlePageChange('AboutMe')}
+                            className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+                        >
+                            About Me
+                        </a>
+                    </span>
+                    </li>
+                    <li className="nav-item"><span>
+                        <a
+                            href="#projects"
+                            onClick={() => handlePageChange('Projects')}
+                            className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Projects
+                        </a>
+                    </span>
+                    </li>
+                    <li className="nav-item"><span>
+                        <a
+                            href="#contact"
+                            onClick={() => handlePageChange('Contact')}
+                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Contact
+                        </a>
+                    </span>
+                    </li><li className="nav-item">
+                    </li>
+                </ul>
+            </div>
+        </section>
     )
 }
 
--- portfolio/src/page/AboutMe.js
+++ portfolio/src/page/AboutMe.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import AvatarBio from '../components/AvatarBio';
-// import NavBar from '../components/NavBar'
 
 function AboutMe() {
     return (
         <div>
-            {/* <NavBar /> */}
             <AvatarBio />
         </div>
 
