58845f81a4581cfd18e04a1753cd15cf8a0e5190
bleufox <thebleufox8@gmail.com>
2021-10-24 23:27:49 -0700

    Fixed small grid sizing issue

--- portfolio/src/page/Contact.js
+++ portfolio/src/page/Contact.js
@@ -9,29 +9,29 @@ function Projects() {
             </div>
             <section id="portfolio" className="projects">
                 <div className="container">
-                    <div className="grid">
+                    <div className="contact-grid">
                         <a href="mailto:thebleufox8@gmail.com">
-                            <div className="project">
+                            <div className="contact-project">
                                 <h3>Email</h3>
                             </div>
                         </a>
                         <a href="https://www.linkedin.com/in/bleufox/" target="blank" rel="noreferrer">
-                            <div className="project">
+                            <div className="contact-project">
                                 <h3>LinkedIn</h3>
                             </div>
                         </a>
                         <a href="https://drive.google.com/file/d/1upQ6iPjUo1oXvPEMFhn8QCgEl-7bJ3Cl/view?usp=sharing" target="blank" rel="noreferrer">
-                            <div className="project">
+                            <div className="contact-project">
                                 <h3>Resume</h3>
                             </div>
                         </a>
                         <a href="https://github.com/bleufox" target="blank" rel="noreferrer">
-                            <div className="project">
+                            <div className="contact-project">
                                 <h3>GitHub</h3>
                             </div>
                         </a>
                         <a href="https://www.instagram.com/juniperinwanderland/" target="blank" rel="noreferrer">
-                            <div className="project">
+                            <div className="contact-project">
                                 <h3>Instagram</h3>
                             </div>
                         </a>
